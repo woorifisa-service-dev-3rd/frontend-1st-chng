32f1b016980f089c3bbed83ecdadfcff3c00ad8d
Jeongseokjin <1223tjrwls@naver.com>
2024-07-18 17:51:07 +0900

    확인 바람.

--- public/login_mail.js
+++ public/login_mail.js
@@ -3,11 +3,10 @@ const login_button = document.getElementById("loginbutton")
 
 const xhr = new XMLHttpRequest();
 
-function getAllFromLocalStorage ()
-{
-    
+function getAllFromLocalStorage() {
+
     //이제, 거기서 빼오기
-    
+
     const keys = Object.keys(window.localStorage)
     // console.log(keys)
     //
@@ -15,9 +14,9 @@ function getAllFromLocalStorage ()
     const values = keys.map(key => {
         const value = window.localStorage.getItem(key);
         // console.log("haha",JSON.parse(value))
-        const parsedValue = JSON.parse(value); 
+        const parsedValue = JSON.parse(value);
         // console.log(valuearray[key]);
-        parsedValue.timestamp = key; 
+        parsedValue.timestamp = key;
         return parsedValue;
     });
 
@@ -28,16 +27,15 @@ function getAllFromLocalStorage ()
 
 }
 
-function sendMailRequest(mail)
-{
-    xhr.open('POST','http://127.0.0.1:5000/mail')
+function sendMailRequest(mail) {
+    xhr.open('POST', 'http://127.0.0.1:5000/mail')
     xhr.setRequestHeader('Content-Type', 'application/json');
     const local_storage_array = getAllFromLocalStorage()
     console.log(local_storage_array)
     xhr.send(JSON.stringify(
         {
             email: mail,
-            local_storage_array : local_storage_array
+            local_storage_array: local_storage_array
 
         }
     )) //여기에 보낼 뭐 좀이따 추가

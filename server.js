3754ab63cc32a5d910e9c195cdda814a384d5d0c
gusdk19 <hyuna030619@naver.com>
2024-07-18 17:31:41 +0900

    feat: 환율 js 구현 완료

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ import HTTP from 'superagent';
 import { config } from 'dotenv';
 config();
 
+// SSL 인증서 검증 비활성화
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const app = express();
 
 app.use(express.static('public'));
 app.use(json());
 
-// const url = process.env.URL;
-// const authKey = process.env.KEY;
-
-const url = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON";
-const authKey = "VNQFbv2fWY2SVWFznbobAXOxuCQP7Y8u";
+const url = process.env.URL;
+const authKey = process.env.KEY;
+const targetCurrencies = ["KRW", "USD", "EUR", "JPY(100)", "CNH"];
 
 // if (!url || !authKey) {
 //     console.error('URL or AUTH_KEY is not defined in .env file');
@@ -21,31 +22,51 @@ const authKey = "VNQFbv2fWY2SVWFznbobAXOxuCQP7Y8u";
 //     console.log('URL, KEY 잘 받았음 ~~', url, authKey);
 // }
 
-const getExchangeRate = async () => {
-    console.log("getExchange 함수 실행 시작");
+let exchangeRates = {};
+
+const fetchExchangeRates = async () => {
+    // console.log("getExchange 함수 실행 시작");
     const searchDate = new Date().toISOString().split('T')[0].replace(/-/g, ''); // yyyyMMdd 형식
     const dataType = "AP01";
 
+    // const query = {
+    //     "authkey": authKey,
+    //     "searchdate": searchDate,
+    //     "data": dataType
+    //   }
+
     try {
-        const res = await HTTP.get(url)
+        const res = await HTTP.post(url)
             .query({ authkey: authKey, searchdate: searchDate, data: dataType });
+
+        // console.log(`Response received: ${res.text}`);
+
+        // const exchangeRateInfoList = JSON.parse(res.text)
+        // let exchangeRate = null;
+
+        // for (const exchangeRateInfo of exchangeRateInfoList) {
+        //     if (exchangeRateInfo.cur_unit === 'USD') {
+        //         exchangeRate = parseFloat(exchangeRateInfo.deal_bas_r.replace(/,/g, ''));
+        //         break;
+        //     }
+        // }
+
+        // if (exchangeRate === null) {
+        //     exchangeRate = 1.0; // 기본 환율 설정
+        // }
+
+        // return exchangeRate;
 
         const exchangeRateInfoList = JSON.parse(res.text);
 
-        let exchangeRate = null;
-
-        for (const exchangeRateInfo of exchangeRateInfoList) {
-            if (exchangeRateInfo.cur_unit === 'USD') {
-                exchangeRate = parseFloat(exchangeRateInfo.deal_bas_r.replace(/,/g, ''));
-                break;
+        exchangeRateInfoList.forEach(exchangeRateInfo => {
+            const currency = exchangeRateInfo.cur_unit;
+            if (targetCurrencies.includes(currency)) {
+                exchangeRates[currency] = parseFloat(exchangeRateInfo.deal_bas_r.replace(/,/g, ''));
             }
-        }
+        });
 
-        if (exchangeRate === null) {
-            exchangeRate = 1.0; // 기본 환율 설정
-        }
-
-        return exchangeRate;
+        console.log('Fetched exchange rates:', exchangeRates);
     } catch (error) {
         console.error(error);
         return null;
@@ -56,17 +77,35 @@ app.get('/', (_, response) => {
     response.sendFile('./index.html');
 })
 
-app.post('/exchange-rate', async (_, response) => {
-    const rate = await getExchangeRate();
-    if (rate !== null) {
-        console.log('rate 받아와');
-        response.json({ rate });
+app.post('/exchange-rate', async (request, response) => {
+    // console.log('POST: /exchange-rate');
+    // console.log('body: ', request.body);
+    // const rate = await getExchangeRate();
+    // if (rate !== null) {
+    //     console.log('rate 받아와', rate);
+    //     response.json({ rate });
+    // } else {
+    //     response.status(500).json({ error: 'Failed to fetch exchange rate' });
+    // }
+    const { fromCurrency, toCurrency, amount } = request.body;
+
+    const fromRate = exchangeRates[fromCurrency];
+    const toRate = exchangeRates[toCurrency];
+
+    console.log('fromCurrency:', fromCurrency);
+    console.log('toCurrency', toCurrency);
+    console.log('amount', amount);
+
+    if (fromRate && toRate) {
+        const convertedAmount = (amount / fromRate) * toRate;
+        response.json({ convertedAmount });
     } else {
         response.status(500).json({ error: 'Failed to fetch exchange rate' });
     }
 });
 
 const port = 5000;
-app.listen(port,
-    () => console.log(`http://127.0.0.1:${port}/ 실행중...`)
-);
+app.listen(port, async () => {
+    await fetchExchangeRates();
+    console.log(`http://127.0.0.1:${port}/ 실행중...`);
+});
